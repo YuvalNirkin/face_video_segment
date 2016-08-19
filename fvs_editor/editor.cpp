1fce36fb9c62b105490e4e490402003e3754568f
Yuval Nirkin <Yuval Nirkin>
2016-08-19 10:50:34 +0300

    Added fvs_editor

---
+++ fvs_editor/editor.cpp
@@ -0,0 +1,258 @@
+// Copyright (c) 2010-2014, The Video Segmentation Project
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the The Video Segmentation Project nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// ---
+
+#include "editor.h"
+
+// std
+#include <exception>
+#include <iostream> // debug
+
+// sfl
+#include <sfl/sequence_face_landmarks.h>
+#include <sfl/utilities.h>
+
+// segmentation
+#include <segment_util/segmentation_io.h>
+#include <segment_util/segmentation_util.h>
+#include <segment_util/segmentation_render.h>
+
+// OpenCV
+#include <opencv2/highgui.hpp>
+
+// Qt
+#include <QLabel>
+#include <QSlider>
+#include <QGridLayout>
+#include <QMouseEvent>
+#include <QCoreApplication>
+
+namespace fvs
+{
+    Editor::Editor(const std::string & video_file, const std::string & seg_file,
+        const std::string& landmarks_file, const std::string & output_dir) :
+        m_loop(true),
+        m_refresh(true),
+        m_slider_pause(false),
+        m_update_pending(false),
+        m_curr_frame_ind(0),
+        m_frame_width(0), m_frame_height(0),
+        m_fps(0),
+        m_total_frames(0)
+    {
+        // Initialize video capture
+        m_cap.reset(new cv::VideoCapture());
+        if (!m_cap->open(video_file))
+            throw std::runtime_error("Failed to open video file!");
+
+        m_frame_width = (int)m_cap->get(cv::CAP_PROP_FRAME_WIDTH);
+        m_frame_height = (int)m_cap->get(cv::CAP_PROP_FRAME_HEIGHT);
+        m_fps = m_cap->get(cv::CAP_PROP_FPS);
+        m_total_frames = (size_t)m_cap->get(cv::CAP_PROP_FRAME_COUNT);
+        m_scaled_frame.reset(new cv::Mat(m_frame_height, m_frame_width, CV_8UC3));
+
+        // Make Qt image.
+        m_render_image.reset(new QImage((const uint8_t*)m_scaled_frame->data,
+            m_scaled_frame->cols,
+            m_scaled_frame->rows,
+            m_scaled_frame->step[0],
+            QImage::Format_RGB888));
+
+        // Initialize sequence face landmarks
+        m_sfl = sfl::SequenceFaceLandmarks::create(landmarks_file);
+        if(m_sfl->size() < m_total_frames)
+            throw std::runtime_error(
+                "The number of landmark frames does not match the number of video frames!");
+        const std::list<std::unique_ptr<sfl::Frame>>& sfl_frames_list =  m_sfl->getSequence();
+        m_sfl_frames.reserve(sfl_frames_list.size());
+        for (auto& frame : sfl_frames_list)
+            m_sfl_frames.push_back(frame.get());  
+
+        // Initialize segmentation reader
+        m_seg_reader.reset(new segmentation::SegmentationReader(seg_file));
+        if (!m_seg_reader->OpenFileAndReadHeaders())
+            throw std::runtime_error("Failed to read segmentation file!");
+        m_seg_desc.reset(new segmentation::SegmentationDesc);
+
+        // Create main widget
+        m_main_widget = new QLabel(this);
+        setCentralWidget(m_main_widget);
+        setWindowTitle("Face Video Segment Editor");
+
+        // Create simple widget used for display.
+        m_display_widget = new QLabel(this);
+        m_display_widget->installEventFilter(this);
+        
+
+        // Frame slider
+        m_frame_slider = new QSlider(Qt::Horizontal);
+        m_frame_slider->setMinimum(0);
+        m_frame_slider->setMaximum(m_total_frames - 1);
+        m_frame_slider->setTickPosition(QSlider::NoTicks);
+        m_frame_slider->setValue(m_curr_frame_ind);
+        connect(m_frame_slider, SIGNAL(valueChanged(int)), this, SLOT(frameIndexChanged(int)));
+        connect(m_frame_slider, SIGNAL(sliderPressed()), this, SLOT(frameSliderPress()));
+        connect(m_frame_slider, SIGNAL(sliderReleased()), this, SLOT(frameSliderRelease()));
+
+        // Hierarchy level slider.
+        m_max_slider_level = 20;
+        m_curr_slider_level = 0;
+        m_hierarchy_slider = new QSlider(Qt::Horizontal);
+        m_hierarchy_slider->setMinimum(0);
+        m_hierarchy_slider->setMaximum(m_max_slider_level);
+        m_hierarchy_slider->setTickPosition(QSlider::TicksBelow);
+        m_hierarchy_slider->setValue(m_curr_slider_level);
+        //connect(m_hierarchy_slider, SIGNAL(sliderMoved(int)), this, SLOT(ChangeLevel(int)));
+        connect(m_hierarchy_slider, SIGNAL(valueChanged(int)), this, SLOT(hierarchyLevelChanged(int)));
+
+        // GUI layout.
+        QGridLayout* centralLayout = new QGridLayout;
+        centralLayout->addWidget(m_display_widget);
+        centralLayout->addWidget(m_frame_slider);
+        centralLayout->addWidget(m_hierarchy_slider);
+        centralLayout->setAlignment(Qt::Alignment(Qt::AlignTop));
+        m_main_widget->setLayout(centralLayout);
+
+        // Resize
+        const int border = 11 * 2;
+        //m_main_widget->resize(m_frame_width + border, m_frame_height + 4 * border);
+        //resize(m_frame_width + border, m_frame_height + 4 * border);
+        //m_display_widget->resize(m_frame_width, m_frame_height);
+
+        QSize frame_slider_size = m_frame_slider->minimumSizeHint();
+        QSize hierarchy_slider_size = m_frame_slider->minimumSizeHint();
+        QSize window_size(m_frame_width + border, m_frame_height + 2*border +
+            frame_slider_size.height() + hierarchy_slider_size.height());
+        m_display_widget->resize(m_frame_width, m_frame_height);
+        m_display_widget->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed));
+        m_main_widget->resize(window_size);
+        resize(window_size);
+
+    }
+
+    Editor::~Editor()
+    {
+    }
+
+    bool Editor::event(QEvent * event)
+    {
+        switch (event->type())
+        {
+        case QEvent::UpdateRequest:
+            m_update_pending = false;
+            update();
+            return QMainWindow::event(event);
+        default:
+            return QMainWindow::event(event);
+        };
+    }
+
+    bool Editor::eventFilter(QObject * object, QEvent * event)
+    {
+        //std::cout << "eventFilter" << std::endl;
+
+        if (object == m_display_widget && event->type() == QEvent::MouseButtonPress)
+        {
+            QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
+            QPoint pos = mouseEvent->pos();
+            std::cout << "pos = (" << pos.x() << ", " << pos.y() << ")" << std::endl;
+            return true;
+        }
+        return false;
+
+        //return QMainWindow::eventFilter(object, event);
+    }
+
+    void Editor::update()
+    {
+        if (!m_refresh) return;
+        //std::cout << "update" << std::endl;
+        m_curr_frame_ind = (int)m_cap->get(cv::CAP_PROP_POS_FRAMES);
+        m_frame_slider->setValue(m_curr_frame_ind);
+        //std::cout << "(" << m_curr_frame_ind << ", " << m_total_frames - 1 << ")" << std::endl;
+        if (m_curr_frame_ind < m_total_frames && m_cap->read(*m_scaled_frame))
+        {
+            render(*m_scaled_frame);
+            m_display_widget->setPixmap(QPixmap::fromImage(m_render_image->rgbSwapped()));
+            m_display_widget->update();
+            if (m_loop) updateLater();
+            else m_refresh = false;
+        }
+        else pause(true);
+    }
+
+    void Editor::updateLater()
+    {
+        if (!m_update_pending) {
+            m_update_pending = true;
+            QCoreApplication::postEvent(this, new QEvent(QEvent::UpdateRequest));
+        }
+    }
+
+    void Editor::seek(int index)
+    {
+        m_cap->set(cv::CAP_PROP_POS_FRAMES, (double)index);
+        m_refresh = true;
+    }
+
+    void Editor::pause(bool pause)
+    {
+        m_loop = !pause;
+        m_refresh = m_loop;
+    }
+
+    void Editor::render(cv::Mat& frame)
+    {
+        sfl::render(frame, *m_sfl_frames[m_curr_frame_ind]);
+    }
+
+    void Editor::frameIndexChanged(int n)
+    {
+        //m_curr_frame_ind = n;
+        //m_frame_slider->setValue(m_curr_frame_ind);
+        seek(n);
+    }  
+
+    void Editor::hierarchyLevelChanged(int n) {
+        m_curr_slider_level = n;
+        m_hierarchy_slider->setValue(m_curr_slider_level);
+    }
+
+    void Editor::frameSliderPress()
+    {
+        std::cout << "press" << std::endl;
+        m_slider_pause = !m_loop;
+        pause(true);
+    }
+
+    void Editor::frameSliderRelease()
+    {
+        std::cout << "release" << std::endl;
+        pause(m_slider_pause);
+        m_slider_pause = false;
+    }
+}   // namespace fvs
---
+++ fvs_editor/editor.h
@@ -0,0 +1,119 @@
+// Copyright (c) 2010-2014, The Video Segmentation Project
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the The Video Segmentation Project nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// ---
+
+#ifndef __FVS_EDITOR_H__
+#define __FVS_EDITOR_H__
+
+#include <memory>
+#include <QMainWindow>
+
+// Forward declarations
+class QLabel;
+class QImage;
+class QSlider;
+class QMouseEvent;
+class QKeyEvent;
+
+namespace cv
+{
+    class Mat;
+    class VideoCapture;
+}
+
+namespace sfl
+{
+    class SequenceFaceLandmarks;
+    class Frame;
+}
+
+namespace segmentation
+{
+    class SegmentationReader;
+    class SegmentationDesc;
+}
+
+
+namespace fvs
+{
+
+	class Editor : public QMainWindow
+	{
+        Q_OBJECT
+
+	public:
+        explicit Editor(const std::string& video_file, const std::string& seg_file,
+            const std::string& landmarks_file, const std::string& output_dir = "");
+        ~Editor();
+
+    protected:
+        bool event(QEvent *event) Q_DECL_OVERRIDE;
+        bool eventFilter(QObject * object, QEvent * event) Q_DECL_OVERRIDE;
+
+        void update();
+        void updateLater();
+        void seek(int index);
+        void pause(bool pause);
+        void render(cv::Mat& frame);
+
+    public slots:
+        void frameIndexChanged(int);
+        void frameSliderPress();
+        void frameSliderRelease();
+        void hierarchyLevelChanged(int);
+        
+    private:
+        QLabel* m_main_widget;
+        QLabel* m_display_widget;
+        QSlider* m_frame_slider;
+        QSlider* m_hierarchy_slider;
+
+        bool m_loop;
+        bool m_refresh;
+        bool m_slider_pause;
+        bool m_update_pending;
+
+        int m_curr_frame_ind;
+        int m_curr_slider_level;
+        int m_max_slider_level;  
+
+        std::unique_ptr<cv::VideoCapture> m_cap;
+        std::unique_ptr<cv::Mat> m_scaled_frame;
+        std::unique_ptr<QImage> m_render_image;
+        std::shared_ptr<sfl::SequenceFaceLandmarks> m_sfl;
+        std::vector<sfl::Frame*> m_sfl_frames;
+        std::unique_ptr<segmentation::SegmentationReader> m_seg_reader;
+        std::unique_ptr<segmentation::SegmentationDesc> m_seg_desc;  
+
+        int m_frame_width, m_frame_height;
+        double m_fps;
+        size_t m_total_frames;
+	};
+
+}   // namespace fvs
+
+#endif  // __FVS_EDITOR_H__
